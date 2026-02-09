84a7a6600198addd88009198fc5ff30c7574a6d5
Alexandr Danilin <alexandr.danilin@ooma.com>
2026-02-09 11:57:55 +0300

    update cyberpunk theme

--- cyberpunk.sh
+++ cyberpunk.sh
@@ -16,5 +16,17 @@ cyber_log()  { echo -e "${CYBER_D}[$(date +%H:%M:%S)]${CYBER_X} ${CYBER_C}▸${C
 cyber_ok()   { echo -e "${CYBER_D}[$(date +%H:%M:%S)]${CYBER_X} ${CYBER_G}✔${CYBER_X} $1"; }
 cyber_warn() { echo -e "${CYBER_D}[$(date +%H:%M:%S)]${CYBER_X} ${CYBER_Y}⚠${CYBER_X} $1"; }
 cyber_err()  { echo -e "${CYBER_D}[$(date +%H:%M:%S)]${CYBER_X} ${CYBER_R}✖${CYBER_X} $1"; }
-cyber_step() { echo -e "\n${CYBER_M}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${CYBER_X}"; echo -e "${CYBER_B}${CYBER_C}⚡ $1${CYBER_X}"; echo -e "${CYBER_M}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${CYBER_X}\n"; }
+cyber_step() {
+  local text="⚡ $1"
+  # +1: ⚡ занимает 2 колонки в терминале, а ${#} считает за 1
+  local len=$(( ${#text} + 1 ))
+  # Ограничиваем: минимум 20, максимум 78 символов
+  (( len < 20 )) && len=20
+  (( len > 78 )) && len=78
+  local line=""
+  for ((i = 0; i < len; i++)); do line+="━"; done
+  echo -e "\n${CYBER_M}${line}${CYBER_X}"
+  echo -e "${CYBER_B}${CYBER_C}${text}${CYBER_X}"
+  echo -e "${CYBER_M}${line}${CYBER_X}\n"
+}
 cyber_trap() { echo -e "\n${CYBER_R}⚡ TERMINATED BY USER ⚡${CYBER_X}"; exit 0; }
